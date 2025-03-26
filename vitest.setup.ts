a2a4c73f31e61690c40b73530128b3c2f072bc10
Edwin WB Li <a15277019572@aliyun.com>
2025-03-26 16:30:28 +0800

    feat: 调整 CI 文件 feat: 调整测试用例配置文件

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,10 +1,26 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import { afterEach, vi } from 'vitest';
 
 vi.mock('@/assets/**/*.{png,png}', () => 'mock-file-stub');
 vi.mock('!!raw-loader!./path/to/file', () => 'raw-content');
 vi.mock('@/assets/**/*.svg', () => 'svg-mock');
+
+vi.mock('react-i18next', () => ({
+	useTranslation: () => ({
+		t: (key: string) => key, // 返回 key 本身
+		i18n: {
+			changeLanguage: vi.fn(),
+		},
+	}),
+	Trans: ({ children }: { children: React.ReactNode }) => children,
+	initReactI18next: {
+		type: '3rdParty',
+		init: vi.fn(),
+	},
+}));
 
 // 全局 DOM 清理
 afterEach(() => {
@@ -19,6 +35,22 @@ if (typeof window.URL.createObjectURL === 'undefined') {
 		value: () => 'mock-blob-url',
 	});
 }
+
+i18n.use(initReactI18next).init({
+	lng: 'en', // 默认语言
+	fallbackLng: 'en', // 回退语言
+	resources: {
+		en: {
+			translation: {
+				// 添加你的翻译内容
+				'Slash Admin': 'Slash Admin',
+			},
+		},
+	},
+	interpolation: {
+		escapeValue: false, // 不转义 HTML
+	},
+});
 
 // Mock matchMedia
 window.matchMedia = (query) => ({
